6c11417016d29b67539d59ff9407e2047a136d61
rashid omarov <art89674010881@mail.ru>
2022-12-27 09:32:49 +0300

    dialogs

--- rashid/src/components/dialogs/dialogs.jsx
+++ rashid/src/components/dialogs/dialogs.jsx
@@ -1,10 +1,20 @@
 import React from "react";
-import './dialogs.css';
+import d from './dialogs.module.css';
 
 const Dialogs = (props) =>{
     return (
-        <div className="text-center">
-            Dialogs
+        <div className={d.dialogs}>
+         <div className={d.dialogsItems}>
+            <div className={d.dialog}>Русик</div>
+            <div className={d.dialog}>Ислам</div>
+            <div className={d.dialog}>Рашид</div>
+            <div className={d.dialog}>Арсик</div>
+         </div>
+         <div className={d.messages}>
+            <div className={d.message}>привет</div>
+            <div className={d.message}>пидараз</div>
+            <div className={d.message}>как дела ?</div>
+         </div>
         </div>
     )
 }
